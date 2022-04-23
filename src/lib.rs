1dd8083b53c268a374cf8377a1c82239c72c1264
mohammed saudi <m.hassan@ogr.iu.edu.tr>
2022-04-23 14:32:34 +0300

    fixed minting logic issue

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use near_contract_standards::non_fungible_token::metadata::{
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
+use near_sdk::collections::{LazyOption, UnorderedSet};
 use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
@@ -38,6 +38,7 @@ enum StorageKey {
     TokenMetadata,
     Enumeration,
     Approval,
+    TokensPerOwner { account_hash: Vec<u8> },
 }
 
 #[near_bindgen]
@@ -82,10 +83,27 @@ impl Contract {
         &mut self,
         token_id: TokenId,
         receiver_id: ValidAccountId,
-        token_metadata: TokenMetadata,
-    ) -> Token {
+        token_metadata: Option<TokenMetadata>,
+    ) -> String {
+        let owner_id: AccountId = receiver_id.into();
+
+        self.tokenizer.owner_by_id.insert(&token_id, &owner_id);
         self.tokenizer
-            .mint(token_id, receiver_id, Some(token_metadata))
+            .token_metadata_by_id
+            .as_mut()
+            .and_then(|by_id| by_id.insert(&token_id, &token_metadata.as_ref().unwrap()));
+
+        if let Some(tokens_per_owner) = &mut self.tokenizer.tokens_per_owner {
+            let mut token_ids = tokens_per_owner.get(&owner_id).unwrap_or_else(|| {
+                UnorderedSet::new(StorageKey::TokensPerOwner {
+                    account_hash: env::sha256(owner_id.as_bytes()),
+                })
+            });
+            token_ids.insert(&token_id);
+            tokens_per_owner.insert(&owner_id, &token_ids);
+        }
+
+        "Done".to_string()
     }
 }
 
