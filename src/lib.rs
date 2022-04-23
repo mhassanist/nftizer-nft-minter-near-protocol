958d7925f07becccb4a7bed1bbc23212b927bdaf
mohammed saudi <m.hassan@ogr.iu.edu.tr>
2022-04-23 10:48:22 +0300

    updated docs and readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-/*
-This contract offers the functionality of minting and NFT. It does not keep track of minted NFTs.
-*/
-
+///This contract offers the functionality of minting non-fungible-tokens (NFT)
+///Users of this contracr can mint NFTs to their own accounts.
+///The minted NFTs will show up in the account that's passed to the contract as receiver_id.
+//Required types for the contract to work.
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
@@ -19,17 +19,19 @@ near_sdk::setup_alloc!();
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    //tokenizer handles creating tokens and minting it
+    //tokenizer handles minting tokens
     tokenizer: NonFungibleToken, //NonFungibleToken type contains all logic for managing tokens
 
-    //metadata is the metadata of the contract
-    //LazyOption prevents a contract from deserializing the metadata until it's needed
-    metadata: LazyOption<NFTContractMetadata>,
+    //metadata is the metadata of the contract (name, description, icon, etc)
+    //that appears in user's wallet when we use this contract for minting
+    metadata: LazyOption<NFTContractMetadata>, //LazyOption prevents a contract from deserializing the metadata until it's needed
 }
 
+//Contract icon in svg-data format. It appears in user's wallet when we use this contract for minting
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,<svg  xmlns='http://www.w3.org/2000/svg'><circle r='50' cx='50' cy='50' fill='tomato'/><circle r='41' cx='47' cy='50' fill='orange'/><circle r='33' cx='48' cy='53' fill='gold'/><circle r='25' cx='49' cy='51' fill='yellowgreen'/><circle r='17' cx='52' cy='50' fill='lightseagreen'/><circle r='9' cx='55' cy='48' fill='teal'/></svg>";
 
 #[derive(BorshSerialize, BorshStorageKey)]
+//managing storage keys
 enum StorageKey {
     NonFungibleToken,
     Metadata,
@@ -74,6 +76,7 @@ impl Contract {
     }
 
     //mint an NFT
+    //passing a token id, receiver account id, and token metadata
     #[payable]
     pub fn nft_mint(
         &mut self,
@@ -86,10 +89,12 @@ impl Contract {
     }
 }
 
-near_contract_standards::impl_non_fungible_token_core!(Contract, tokenizer); //to get the mint function implementation working
+//required implementation from the nft standard library for the minting process to work
+near_contract_standards::impl_non_fungible_token_core!(Contract, tokenizer);
 near_contract_standards::impl_non_fungible_token_approval!(Contract, tokenizer);
 near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokenizer);
 
+//returns the contract metadata
 #[near_bindgen]
 impl NonFungibleTokenMetadataProvider for Contract {
     fn nft_metadata(&self) -> NFTContractMetadata {
